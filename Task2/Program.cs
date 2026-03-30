745028b966323505666a1378f6aa940006827c30
senbet22 <senbet22@users.noreply.github.com>
2026-03-30 11:48:05 +0200

    Task2 completed

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+Calculator calculator = new Calculator();
+
+
+Console.WriteLine("Welcome to Simple Calculator!");
+Console.WriteLine("Please enter your first number:");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter your second number:");
+double b = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Choose your operation: A to add, S to subtract, M to multiply or D to Devide");
+string operation = Console.ReadLine().ToUpper();
+
+
+double result;
+
+switch (operation) 
+{ 
+    case "A":
+        result = calculator.Add(a, b);
+        Console.WriteLine($"Result: {result}");
+        break;
+    case "S":
+        result = calculator.Subtract(a, b);
+        Console.WriteLine($"Result: {result}");
+        break;
+    case "M":
+        result = calculator.Multiply(a, b);
+        Console.WriteLine($"Result: {result}");
+        break;
+    case "D":
+        result = calculator.Divide(a, b);
+        Console.WriteLine($"result: {result}");
+        break;
+    default:
+        Console.WriteLine("Invalid Operation.");
+        break;
+}
+
+
+
+
--- Task2/calculator.cs
+++ Task2/calculator.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Methods : Add, Subtract, Multiply, Divide. 
 
-namespace Task2
+public class Calculator
 {
-    internal class calculator
+    public double Add(double a, double b)
     {
+        return a + b;
     }
+
+    public double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            return double.NaN;
+        }
+        return a / b;
+    }
+
 }
+
