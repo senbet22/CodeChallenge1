96d6f9f233534a29ab0019b5d86c02a3231ac51c
senbet22 <senbet22@users.noreply.github.com>
2026-03-30 11:11:25 +0200

    Task1 has been completed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello world!");
+﻿string name = "Senbet";
+int age = 27;
+double height = 1.85;
+char grade = 'A';
+bool isStudent = true;
+
+Console.WriteLine($"Hey! My name is {name}, i am {age} years old. Some people think i am short but my passport says i am {height}cm. " +
+    $"Currently i am a student at Kodehode: {isStudent} and strive to be a grade {grade} student.");
+Console.ReadKey();
